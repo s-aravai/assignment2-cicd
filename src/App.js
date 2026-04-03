cf2db4247bd93246c2f485271ec04ed70e806112
Salman Aravai <salmanaravai786@gmail.com>
2026-04-03 12:44:53 -0600

    Add course name to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+jsximport React from 'react';
 
 function App() {
   return (
     <div>
       <h1>Salman Aravai</h1>
+      <p>TECH2102:Enterprise Computing-26JANMNOS3</p>
     </div>
   );
 }
